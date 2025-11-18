c93fa9b103cd5377558d5028b520fab651b3f320
Dumr3n <k3nteck@gmail.com>
2025-11-18 18:51:42 +0000

    Longest consecutive sequence

---
+++ CPP/LongestConsecutiveSequence.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int longestConsecutive(vector<int>& nums) {
+     if (nums.size() < 2)
+        return nums.size();
+    
+     set<int> map;
+     for (auto i : nums) 
+        map.insert(i);
+    int maxLen = 0;
+    int currLen = 1;
+    auto it = map.begin();
+    int currI = *it;
+    it++;
+    while (it != map.end()) {
+        if (*it == currI+1) {
+            currLen++;
+        } else {
+            maxLen = max(currLen, maxLen);
+        }
+        currI = *it;
+        it++;
+    }
+    return max(currLen, maxLen);
+}
+int main()
+{
+    vector<int> nums{9,1,4,7,3,-1,0,5,8,-1,6};
+    
+    cout << "Longest consecutive sequence length " << longestConsecutive(nums) << endl;
+    return 0;
+}
