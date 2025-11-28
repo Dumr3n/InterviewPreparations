c9457a89a34df43558e1dcc17e87c2d77eec9c6c
Dumr3n <k3nteck@gmail.com>
2025-11-28 11:20:43 +0000

    Lowest Common Ancestor of a binary tree

---
+++ CPP/LowestCommonAncestorOfABinaryTree.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <deque>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int val) : val(val), left(nullptr), right(nullptr) {}
+    TreeNode(int val, TreeNode* left, TreeNode* right) : val(val), left(left), right(right) {}
+};
+
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    if (!root || root == p || root == q) {
+        return root;
+    }
+    TreeNode* left = lowestCommonAncestor(root->left, p, q);
+    TreeNode* right = lowestCommonAncestor(root->right, p, q);
+
+    if (left && right) {
+        return root;
+    }
+    return left != nullptr ? left : right;
+}
+
+void PreOrderPrint(TreeNode* root) {
+    if (!root) {
+        return;
+    }
+    cout << root->val << " ";
+    PreOrderPrint(root->left);
+    PreOrderPrint(root->right);
+}
+
+int main()
+{
+    TreeNode* p = new TreeNode(5, new TreeNode(6), new TreeNode(2, new TreeNode(7), new TreeNode(4)));
+    TreeNode* q = new TreeNode(1, new TreeNode(0), new TreeNode(8));
+    TreeNode* root = new TreeNode(3, p, q);
+
+    cout << "Ans is " << lowestCommonAncestor(root, p, q)->val << endl;
+    return 0;
+}
